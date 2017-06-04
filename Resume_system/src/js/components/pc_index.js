875e3dd52a9fb59e3076b1cf5fb227387de16f28
truejasonfans <472408570@qq.com>
2017-06-04 10:53:53 +0800

    resolve some conflicts and import router

--- Resume_system/src/js/components/pc_index.js
+++ Resume_system/src/js/components/pc_index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PCHeader from './pc_header';
 import PCFooter from './pc_footer';
+import PCResumeContainer from './pc_resume_container';
 export default class PCIndex extends React.Component {
 	render() {
 		return (
 			<div>
 				<PCHeader></PCHeader>
+				<PCResumeContainer></PCResumeContainer>
 				<PCFooter></PCFooter>
 			</div>
 		);
--- Resume_system/src/js/root.js
+++ Resume_system/src/js/root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
-import {Button} from 'antd';
+import { Router, Route, hashHistory } from 'react-router';
+import { Button } from 'antd';
 import PCIndex from './components/pc_index';
 import MobileIndex from './components/mobile_index';
 import 'antd/dist/antd.css';
@@ -11,14 +11,16 @@ export default class Root extends React.Component {
 		return (
 			<div>
 				<MediaQuery query='(min-device-width: 1224px)'>
-					<PCIndex/>
+					<Router history = {hashHistory}>
+						<Route path ="/" component={PCIndex}/>
+					</Router>
 				</MediaQuery>
 				<MediaQuery query='(max-device-width: 1224px)'>
-					<MobileIndex/>
+					<MobileIndex />
 				</MediaQuery>
 			</div>
 		);
 	};
 }
 ReactDOM.render(
-	<Root/>, document.getElementById('mainContainer'));
+	<Root />, document.getElementById('mainContainer'));
