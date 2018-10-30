39f494e9b1e7456a1b60e98d209f30ff04671a85
truejasonfans <472408570@qq.com>
2018-10-30 23:36:25 +0800

    refacby the umi;

--- Resume_system/resume_system/src/components/header/login_model.js
+++ Resume_system/resume_system/src/components/header/login_model.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import { Modal, Tabs, Form,Input, Button } from 'antd';
+import { Modal, Tabs, Form, Input, Button } from 'antd';
 const LoginModel = (props) => (
-  <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={true} onCancel={() => this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText="关闭">
-    <Tabs type="card" onChange={props.callback.bind(this)}>
+  <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={true} okText="关闭">
+    {/* <Tabs type="card" onChange={props.callback.bind(this)}>
       <Tabs.TabPane tab="登录" key="1">
         <Form horizontal onSubmit={props.handleSubmit.bind(this)}>
           <Form.item label="账户">
-            <Input placeholder="请输入您的账号" {...props.form('userName')} />
+            <Input placeholder="请输入您的账号"/>
           </Form.item>
           <Form.item label="密码">
-            <Input type="password" placeholder="请输入您的密码" {...props.form('password')} />
+            <Input type="password" placeholder="请输入您的密码"/>
           </Form.item>
           <Button type="primary" htmlType="submit">登录</Button>
         </Form>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="注册" key="2">
+      </Tabs.TabPane> */}
+    {/* <Tabs.TabPane tab="注册" key="2">
         <Form horizontal onSubmit={props.handleSubmit.bind(this)}>
           <Form.item label="账户">
-            <Input placeholder="请输入您的账号" {...props.form('r_userName')} />
+            <Input placeholder="请输入您的账号"/>
           </Form.item>
           <Form.item label="密码">
-            <Input type="password" placeholder="请输入您的密码" {...props.form('r_password')} />
+            <Input type="password" placeholder="请输入您的密码" />
           </Form.item>
           <Form.item label="确认密码">
-            <Input type="password" placeholder="请再次输入您的密码" {...props.form('r_confirmPassword')} />
+            <Input type="password" placeholder="请再次输入您的密码" />
           </Form.item>
           <Button type="primary" htmlType="submit">注册</Button>
         </Form>
       </Tabs.TabPane>
-    </Tabs>
+    </Tabs> */}
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
   </Modal>
 );
 export default LoginModel;
